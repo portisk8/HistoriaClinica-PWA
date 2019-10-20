60e14171c79461b7c2e82465f22c7ca2824cffcb
Leonel <lolo042@hotmail.com>
2019-10-20 17:05:17 -0300

    added PWA

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 
-var port = Number(process.env.PORT || 3000);
+var port = Number(process.env.PORT || 3001);
 
 app.listen(port, function(){
     console.log("Running in port", port);
