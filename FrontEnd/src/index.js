ca42d36c1e4f9777de41f78a228908cfe8ab344c
Leonel <lolo042@hotmail.com>
2019-10-20 18:22:13 -0300

    added routes

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Route, BrowserRouter} from 'react-router-dom';
+import { render } from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+render (
+    <BrowserRouter>
+        <Route path="/" component={App} />
+    
+    </BrowserRouter>,
+    document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
