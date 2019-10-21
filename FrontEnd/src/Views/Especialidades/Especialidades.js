0b80e90c14de5e411d1818d1dcc0b34c3a19b2b3
Augusto Emiliano Portillo <aportillo@desarrollosnea.com>
2019-10-21 15:12:08 -0300

    Vista listado de especialidades con redireccion a una especialidad especifica

--- FrontEnd/src/Views/Especialidades/Especialidades.js
+++ FrontEnd/src/Views/Especialidades/Especialidades.js
@@ -14,13 +14,33 @@ class Especialidades extends React.Component {
     // const especialidades = async () => await getEspecialidades();
     getEspecialidades().then(data => {
       console.log(data);
+      this.setState({ especialidadList: data.items });
     });
     // console.log(especialidades);
   }
+
+  goToEspecialidad = especialidadId => {
+    let path = `especialidad/${especialidadId}`;
+    this.props.history.push(path);
+  };
+
   render() {
     return (
       <div>
         <Header></Header>
+        {this.state.especialidadList.length > 0 &&
+          this.state.especialidadList.map((item, idx) => (
+            <div>
+              <button
+                type="button"
+                class="btn btn-outline-primary btn-lg btn-block"
+                onClick={() => this.goToEspecialidad(idx)}
+                style={{ marginBottom: 10 }}
+              >
+                {item}
+              </button>
+            </div>
+          ))}
       </div>
 
       // <Redirect from="/" to="/dashboard" />
