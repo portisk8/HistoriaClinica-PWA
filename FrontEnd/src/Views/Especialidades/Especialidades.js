df6f9df9b9690b5c3e024aa674c5375440ccfa11
Augusto Emiliano Portillo <aportillo@desarrollosnea.com>
2019-10-24 22:03:07 -0300

    Fix se rompe la vista cuando no se conecta con el backend en especialidades

--- FrontEnd/src/Views/Especialidades/Especialidades.js
+++ FrontEnd/src/Views/Especialidades/Especialidades.js
@@ -14,7 +14,9 @@ class Especialidades extends React.Component {
     // const especialidades = async () => await getEspecialidades();
     getEspecialidades().then(data => {
       console.log(data);
-      this.setState({ especialidadList: data.items });
+      if (data && Array.isArray(data)){
+        this.setState({ especialidadList: data.items });
+      }
     });
     // console.log(especialidades);
   }
@@ -42,19 +44,6 @@ class Especialidades extends React.Component {
             </div>
           ))}
       </div>
-
-      // <Redirect from="/" to="/dashboard" />
-      /* <ul>
-        <li>
-          <Link to="/dashboard">Dashborad</Link>
-        </li>
-        <li>
-          <Link to="/provider">Provider</Link>
-        </li>
-        <li>
-          <Link to="/login">Salir</Link>
-        </li>
-      </ul>*/
     );
   }
 }
