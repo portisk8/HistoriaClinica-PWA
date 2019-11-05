eefcc11c3921642a2089cf0d7c35d475e8bec6ce
Leonel <lolo042@hotmail.com>
2019-11-04 23:56:25 -0300

    login model,controllers and route

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -3,12 +3,15 @@ var app = express();
 var cors = require("cors");
 app.use(cors());
 
+
 // import routes
 const especialidadesRouter = require('./app/src/routes/especialidadesRouter');
 const infoProfesionalRouter = require('./app/src/routes/profesionalesRouter');
+const loginRouter = require('./app/src/routes/loginRouter');
 
 app.use('/api',especialidadesRouter)
 app.use('/api',infoProfesionalRouter)
+app.use('/api',loginRouter)
 
 //config ports
 var port = Number(process.env.PORT || 3001);
