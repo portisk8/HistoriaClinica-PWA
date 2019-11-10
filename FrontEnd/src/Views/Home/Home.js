2fb86c3eb661c279511a87498223a2fa935ec404
CristianSaraceni <torry.90.03@gmail.com>
2019-11-10 13:41:39 -0300

    add slider

--- FrontEnd/src/Views/Home/Home.js
+++ FrontEnd/src/Views/Home/Home.js
@@ -18,6 +18,12 @@ class Home extends React.Component {
   }*/
  
   render() {
+
+
+    /*$('.carousel').carousel({
+      interval: 200
+    })*/
+
     return (
       <div>
         <Header></Header>
@@ -31,6 +37,42 @@ class Home extends React.Component {
           </Link>
         </div>
         
+
+
+
+        <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src={logo} alt="First slide"/>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src={logo} alt="Second slide"/>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src={logo} alt="Third slide"/>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+
+
+
+
+
         
         <Footer></Footer>
       </div>
