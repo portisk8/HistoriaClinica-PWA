25ffb9d21f277794b88a664f58e1bf6b2179fe2b
Leonel <lolo042@hotmail.com>
2019-11-09 21:05:37 -0300

    bcrypt compare

--- BackEnd/app/src/controllers/loginController.js
+++ BackEnd/app/src/controllers/loginController.js
@@ -1,24 +1,18 @@
 const loginModel = require('../models/loginModel');
+var bcrypt = require('bcrypt');
 
 module.exports = {
     login: function (req,res) {
-        let users = {
-
-        }
-
-        loginModel.items.forEach(element => {
-            if(req.body.username == element.username){
-                users = {
-                    username: req.body.username,
-                    password: req.body.password
+        loginModel.getUsers(9).then(user => {
+           // res.send(user[0].pass)
+          bcrypt.compare(req.body.password, user[0].pass, function(err, result){
+                if (err) throw err
+                if (result) {
+                    return res.status(200).json({ msg: "Login success" })
+                } else {
+                    return res.status(401).json({ msg: "Invalid credencial" })
                 }
-            }
-        });
-
-        res.send(users);
-    
-        
-         
-              
+           })
+        })
     }
 }
--- BackEnd/app/src/models/loginModel.js
+++ BackEnd/app/src/models/loginModel.js
@@ -1,3 +1,26 @@
+//include the model (aka DB connection)
+var db = require("./dbConnection");
+
+
+
+//create class
+var Login = {
+  //function to query all items
+  getUsers: (usuarioId) => {
+    const findUser = "SELECT * FROM Usuarios where usuarioId="+usuarioId+";";
+    return new Promise((resolve, reject) => {
+      db.query(findUser, (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res);
+        }
+      });
+    });
+  }
+};
+module.exports = Login;
+/*
 module.exports = Users = {
     items: [{username: "name", password: "123456"}]
-};
+};*/
