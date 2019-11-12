2fa05be8fe98ab12b7020437a1b5bf6edab3d4f9
Ale Bottini <alebottini001@gmail.com>
2019-11-12 20:03:34 -0300

    Profesionales Service (done)

---
+++ FrontEnd/src/Service/ProfesionalesObtener.js
@@ -0,0 +1,16 @@
+import { axiosRequest } from "../utils/request";
+import { API_URL } from "../Common/environment";
+
+const __APIURL = API_URL;
+
+export function getProfesionalesObtener(especialidad) {
+  //   const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+  console.log("call api profesionales");
+  return axiosRequest(`${__APIURL}/api/profesionales/${especialidad}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+      // Authorization: "Bearer " + "currentUser.token"
+    }
+  }).then(response => response.data);
+}
