91b5bd5647d59119f7ace2a73ed69a59e9ea875b
AguirreAgustin <ardagustin14@gmail.com>
2019-11-12 20:42:30 -0300

    Query to DataBase num 7

--- BDD/Script/Consultas/Consultas/Query7.sql
+++ BDD/Script/Consultas/Consultas/Query7.sql
@@ -1,10 +1,14 @@
 -- Listar los nombres, diagnósticos y obra social de los pacientes que estuvieron más tiempo internados durante el primer semestre de 2019.
 
-select  *
-from    Internaciones i, 
-        Pacientes p
-where 	i.paciente = p.numeroHistoriaClinica and 
-		(i.fechaIngreso between "2019-01-01" and "2019-06-30") and
-        (i.fechaSalida between "2019-01-01" and "2019-06-30");
-        
--- Falta saber cual es el que más tiempo estuvo
+select *
+from    Internaciones i
+		inner join Pacientes p
+		on i.paciente = p.numeroHistoriaClinica
+where 	(i.fechaIngreso between "2019-01-01" and "2019-06-30") and
+		(i.fechaSalida between "2019-01-01" and "2019-06-30")
+        and datediff(i.fechaSalida, i.fechaIngreso) = (
+															select  max(datediff(i.fechaSalida, i.fechaIngreso))
+															from    Internaciones i
+															where 	(i.fechaIngreso between "2019-01-01" and "2019-06-30") and
+																	(i.fechaSalida between "2019-01-01" and "2019-06-30")
+															);
