3c3f324e166e90822126363d4ad9ee2a95ba4549
AguirreAgustin <ardagustin14@gmail.com>
2019-11-13 13:48:13 -0300

    HistorialTurnos Procedure  changes

--- BDD/Script/Consultas/Stored Procedures/HistorialTurnos.sql
+++ BDD/Script/Consultas/Stored Procedures/HistorialTurnos.sql
@@ -3,19 +3,23 @@ delimiter //
 create PROCEDURE HistorialTurnos (in dni int)
 BEGIN
 
-    select  pro.especialidad,
-            pro.apellido,
-            pro.nombre,
-            tur.fechaAsignacion,
-            tur.horaAsignacion,
-            tur.fechaAtencion,
-            tur.horaAtencion
-    from    Pacientes pac inner join
-            Turnos tur on 
-            pac.numeroHistoriaClinica = tur.paciente inner join 
-            Profesionales pro on 
-            tur.profesional = pro.numeroMatricula
-    where   pac.numeroDniPaciente = dni;
+    select      pro.especialidad,
+                pro.apellido,
+                pro.nombre,
+                tur.fechaAsignacion,
+                tur.horaAsignacion,
+                year(tur.fechaAsignacion) as anioAsignado,
+                month(tur.fechaAsignacion) as mesAsignado, 
+                day(tur.fechaAsignacion) as diaAsignado,
+                tur.horaAtencion,
+                if((current_date()> tur.fechaAsignacion or current_date() = tur.fechaAsignacion) 
+                        and current_time() > tur.horaAsignacion,"Atendido","No Atendido") as estado
+    from        Pacientes pac inner join 
+                Turnos tur on 
+                pac.numeroHistoriaClinica = tur.paciente  inner join 
+                Profesionales pro on 
+                tur.profesional = pro.numeroMatricula
+    where       pac.numeroDniPaciente = dni;
 
 END
 
