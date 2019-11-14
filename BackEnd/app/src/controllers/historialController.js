e618e9cd2f6c0cdf64288afd648e143dc9847112
portisk8 <aportillo@desarrollosnea.com>
2019-11-14 02:01:51 -0300

    fix build backend

--- BackEnd/app/src/controllers/historialController.js
+++ BackEnd/app/src/controllers/historialController.js
@@ -1,37 +1,35 @@
-const historialRepository = require('../repositories/historialRepository');
+const historialRepository = require("../repositories/historialRepository");
 // const historialTurnos = require("../models/historialTurnosModel");
 
 module.exports = {
-    historialDrogas: function (req, res) {
-       return historialRepository.historialDrogaObtener(req.params.dni).then(data => {         
-            res.send(data)          
-        }).catch(error => {
-            res.send(error)
-        })           
-    },
-    historialTurnos: function(req, res) {
-        return historialRepository
-          .historialTurnosObtener()
-          .then(data => {
-            res.send(data);
-          })
-          .catch(error => {
-            res.send(error);
-          });
-      },
-      obtenerHistoriaClinica: function(req, res) {
-        return historialRepository.historialClinicoObtener(req.params.dni).then(historiaClinica =>{
-            res.send(historiaClinica[0])
-        }).catch(error =>{
-            res.send(error)
-        })
-        
-    }
-
-}
-
-
-
-module.exports = {
- 
+  historialDrogas: function(req, res) {
+    historialRepository
+      .historialDrogaObtener(req.params.dni)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(error => {
+        res.send(error);
+      });
+  },
+  historialTurnos: function(req, res) {
+    historialRepository
+      .historialTurnosObtener()
+      .then(data => {
+        res.send(data);
+      })
+      .catch(error => {
+        res.send(error);
+      });
+  },
+  obtenerHistoriaClinica: function(req, res) {
+    historialRepository
+      .historialClinicoObtener(req.params.dni)
+      .then(historiaClinica => {
+        res.send(historiaClinica[0]);
+      })
+      .catch(error => {
+        res.send(error);
+      });
+  }
 };
--- BackEnd/app/src/routes/historialRouter.js
+++ BackEnd/app/src/routes/historialRouter.js
@@ -1,10 +1,13 @@
-var express = require('express')
-var api = express.Router()
+var express = require("express");
+var api = express.Router();
 
-const historialController = require('../controllers/historialController');
+const historialController = require("../controllers/historialController");
 
-api.get('/historiales/drogas/:dni', historialController.historialDrogas);
-api.get('/historiales/turnos', historialController.historialTurnos);
-api.get('/historiales/clinicos/:dni', historialController.obtenerHistoriaClinica);
+api.get("/historiales/drogas/:dni", historialController.historialDrogas);
+api.get("/historiales/turnos", historialController.historialTurnos);
+api.get(
+  "/historiales/clinicos/:dni",
+  historialController.obtenerHistoriaClinica
+);
 
 module.exports = api;
