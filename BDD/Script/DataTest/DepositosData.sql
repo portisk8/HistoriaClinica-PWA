392afa3444d485792b984f7cbdb13eac2d51a586
AguirreAgustin <ardagustin14@gmail.com>
2019-11-15 01:06:12 -0300

    dataBase changes

--- BDD/Script/DataTest/DepositosData.sql
+++ BDD/Script/DataTest/DepositosData.sql
@@ -1,1 +1,11 @@
-INSERT INTO Depositos (nombreDeposito,telefonoInterno) VALUES ("Est Nunc LLP","0800 355612"),("Fringilla Purus Consulting","(0161) 350 8927"),("Laoreet Posuere Enim Associates","(0118) 154 3713"),("Arcu Corporation","(016781) 10987"),("Hendrerit Foundation","(01057) 14447"),("Blandit PC","0800 1111"),("Ipsum Cursus Foundation","076 0094 8744"),("Inceptos Ltd","(01260) 045910"),("Tellus Eu LLP","(01564) 56572"),("Dui Limited","(0114) 775 3613");
+INSERT INTO Depositos (nombreDeposito,telefonoInterno) VALUES 
+("Est Nunc LLP","0800 355612"),
+("Fringilla Purus Consulting","(0161) 350 8927"),
+("Laoreet Posuere Enim Associates","(0118) 154 3713"),
+("Arcu Corporation","(016781) 10987"),
+("Hendrerit Foundation","(01057) 14447"),
+("Blandit PC","0800 1111"),
+("Ipsum Cursus Foundation","076 0094 8744"),
+("Inceptos Ltd","(01260) 045910"),
+("Tellus Eu LLP","(01564) 56572"),
+("Dui Limited","(0114) 775 3613");
